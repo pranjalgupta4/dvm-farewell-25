488ee450faf93a4add1f2ba6e42db3cdd6a03077
Pranjal Gupta <pranjalgupta58.97@gmail.com>
2025-04-26 04:10:49 +0530

    laptop carousel done

--- components/carousel/Carousel.jsx
+++ components/carousel/Carousel.jsx
@@ -1,5 +1,5 @@
 import styles from "./carousel.module.scss";
-import { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 
@@ -7,19 +7,28 @@ import mainPlate from "../../public/verticals/mainPlate.png";
 import subPlate from "../../public/verticals/subPlate.png";
 import king from "../../public/verticals/virat kohli.png";
 import frame2 from "../../public/verticals/frame2.png";
+import light1 from "../../public/verticals/light1.png";
+import light2 from "../../public/verticals/light2.png";
+
 import leftArrow from "../../public/verticals/leftArrow.svg";
 import rightArrow from "../../public/verticals/rightArrow.svg";
 
 import verticals from "./verticals.js";
 
 function Carousel() {
+  //Declaring variables and Functions
   const { contextSafe } = useGSAP();
 
   const [indx, setindx] = useState(2);
   const [memberIndx, setmemberIndx] = useState(3);
+  const [animatorRight, setAnimatorRight] = useState(true);
+  const [animatorLeft, setAnimatorLeft] = useState(true);
+  const [animator, setAnimator] = useState(false);
+  const [namePlate, setnamePlate] = useState("");
 
   const verticalsRef = useRef([]);
   const membersRef = useRef([]);
+  const namePlateRef = useRef();
 
   const allMembers = verticals.flatMap((vertical) =>
     vertical.members.map((member) => ({
@@ -33,13 +42,8 @@ function Carousel() {
   const switcher = (num) =>
     num < 0 ? num + verticals.length : num > 5 ? num - verticals.length : num;
 
-  // useEffect(() => {
-  //   setindx(allMembers[switcherMem(memberIndx)].index);
-  //   console.log(indx);
-  // }, [memberIndx]);
-
+  //Image Carousel Animation
   useEffect(() => {
-    console.log("memberIndx: ", memberIndx);
     const carouselMover = contextSafe(() => {
       membersRef.current.forEach((el, index) => {
         switch (index) {
@@ -70,35 +74,47 @@ function Carousel() {
             break;
 
           case switcherMem(memberIndx - 1):
-            gsap
-              // .to(el, {
-              //   x: "0%",
-              //   y: "0%",
-              //   z: "-100px",
-              //   filter: "blur(0px) contrast(1) brightness(1)",
-              //   duration: 1,
-              //   ease: "circ.out",
-              // });
+            const tlFirst1 = gsap.timeline({
+              paused: animator && animatorRight,
+            });
+            tlFirst1.to(el, {
+              x: "-45%",
+              y: "-2%",
+              z: "-100px",
+              filter: "blur(1px) contrast(1.2) brightness(0.6)",
+              duration: 1,
+              ease: "circ.out",
+            });
+
+            const tlFirst2 = gsap.timeline({ paused: animatorLeft });
+            tlFirst2
+              .to(el, {
+                x: "-53%",
+                y: "-1%",
+                z: "-50px",
+                filter: "blur(1px) contrast(1.1) brightness(0.8)",
+                duration: 0.5,
+                ease: "sine.inOut",
+              })
               .to(el, {
                 x: "-45%",
                 y: "-2%",
                 z: "-100px",
                 filter: "blur(1px) contrast(1.2) brightness(0.6)",
-                duration: 1,
-                ease: "circ.out",
+                duration: 0.5,
+                ease: "sine.inOut",
               });
             break;
 
           case switcherMem(memberIndx):
-            const tl1 = gsap.timeline();
-            tl1
+            const tlSecond1 = gsap.timeline({ paused: animatorRight });
+            tlSecond1
               .to(el, {
                 x: "-53%",
                 y: "-1%",
                 z: "-50px",
                 filter: "blur(0.5px) contrast(1.1) brightness(0.8)",
                 duration: 0.5,
-                // scale: 0.9,
                 ease: "sine.inOut",
               })
               .to(el, {
@@ -107,20 +123,49 @@ function Carousel() {
                 z: "0px",
                 filter: "blur(0px) contrast(1) brightness(1)",
                 duration: 0.5,
-                ease: "circ.out",
+                ease: "sine.inOut",
               });
+
+            const tlSecond2 = gsap.timeline({ paused: animatorLeft });
+            tlSecond2
+              .to(el, {
+                x: "53%",
+                y: "-1%",
+                z: "-50px",
+                filter: "blur(1px) contrast(1.1) brightness(0.8)",
+                duration: 0.5,
+                ease: "sine.inOut",
+              })
+              .to(el, {
+                x: "0%",
+                y: "0%",
+                z: "0px",
+                filter: "blur(0px) contrast(1) brightness(1)",
+                duration: 0.5,
+                ease: "sine.inOut",
+              });
+            const tlSecond3 = gsap.timeline({
+              paused: animator,
+            });
+            tlSecond3.to(el, {
+              x: "0%",
+              y: "0%",
+              z: "0px",
+              filter: "blur(0px) contrast(1) brightness(1)",
+              duration: 1,
+              ease: "circ.out",
+            });
             break;
 
           case switcherMem(memberIndx + 1):
-            const tl2 = gsap.timeline();
-            tl2
+            const tlThird1 = gsap.timeline({ paused: animatorRight });
+            tlThird1
               .to(el, {
                 x: "53%",
                 y: "-1%",
                 z: "-50px",
                 filter: "blur(0.5px) contrast(1.1) brightness(0.8)",
                 duration: 0.5,
-                // scale: 0.9,
                 ease: "sine.inOut",
               })
               .to(el, {
@@ -129,8 +174,20 @@ function Carousel() {
                 z: "-100px",
                 filter: "blur(1px) contrast(1.2) brightness(0.6)",
                 duration: 0.5,
-                ease: "circ.out",
+                ease: "sine.inOut",
               });
+
+            const tlThird2 = gsap.timeline({
+              paused: animator && animatorLeft,
+            });
+            tlThird2.to(el, {
+              x: "45%",
+              y: "-2%",
+              z: "-100px",
+              filter: "blur(1px) contrast(1.2) brightness(0.6)",
+              duration: 1,
+              ease: "circ.out",
+            });
             break;
 
           case switcherMem(memberIndx + 2):
@@ -173,7 +230,35 @@ function Carousel() {
         }
       });
     });
+
+    //Nameplate Animation
+    const namePlateChanger = contextSafe(() => {
+      if (namePlateRef.current) {
+        const tl = gsap.timeline();
+        tl.set(namePlateRef.current, { opacity: 1 })
+          .to(namePlateRef.current, {
+            opacity: 0,
+            duration: 0.5,
+            ease: "circ.inOut",
+          })
+          .to(namePlateRef.current, {
+            opacity: 1,
+            duration: 0.5,
+            ease: "circ.inOut",
+          });
+      }
+    });
+
+    namePlateChanger();
+    setTimeout(() => {
+      setnamePlate(allMembers[switcherMem(memberIndx)].name);
+    }, 500);
     carouselMover();
+  }, [memberIndx]);
+
+  //Vertical Box Animation
+  useEffect(() => {
+    setindx(allMembers[switcherMem(memberIndx)].index);
   }, [memberIndx]);
 
   useEffect(() => {
@@ -214,45 +299,20 @@ function Carousel() {
     initialPose();
   }, []);
 
-  const verticalMover = contextSafe(() => {
-    verticalsRef.current.forEach((el, index) => {
-      if (index === switcher(indx + 3)) {
-        gsap.set(el, {
-          display: "none",
-        });
-      } else {
-        const targetMap = {
-          [switcher(indx - 2)]: "-10deg",
-          [switcher(indx - 1)]: "-6deg",
-          [switcher(indx + 1)]: "6deg",
-          [switcher(indx + 2)]: "10deg",
-        };
-        const angle = targetMap[index] || "0deg";
-
-        const targetMap2 = {
-          [switcher(indx - 2)]: "-30px",
-          [switcher(indx - 1)]: "-10px",
-          [switcher(indx + 1)]: "-10px",
-          [switcher(indx + 2)]: "-30px",
-        };
-        const zTranslate = targetMap2[index] || "0px";
-
-        gsap.to(el, {
-          display: "flex",
-          z: zTranslate,
-          rotateY: angle,
-          duration: 1,
-          ease: "power2.inOut",
-        });
-      }
-    });
-  });
-
+  //Calling of animation on click of arrows
   const handleClick = (direction) => {
     setmemberIndx((prev) => {
-      return direction === "left"
-        ? switcherMem(prev + 1)
-        : switcherMem(prev - 1);
+      if (direction === "left") {
+        setAnimatorLeft(false);
+        setAnimatorRight(true);
+        setAnimator(true);
+        return switcherMem(prev + 1);
+      } else {
+        setAnimatorLeft(true);
+        setAnimatorRight(false);
+        setAnimator(true);
+        return switcherMem(prev - 1);
+      }
     });
   };
 
@@ -269,6 +329,13 @@ function Carousel() {
             <img src={frame2} alt="frame2" />
           </div>
         ))}
+
+        <p className={styles.namePlate} ref={namePlateRef}>
+          {namePlate}
+        </p>
+
+        <img src={light1} alt="light1" className={styles.light} />
+        <img src={light2} alt="light2" className={styles.light} />
         <img
           src={leftArrow}
           alt="leftArrow"
